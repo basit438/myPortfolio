d7d7cf77353e5803052906385d883f8c85f9393f
Basit <basitmanzoor438@gmail.com>
2024-09-27 15:09:27 +0530

    resolved skill issues

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import 'boxicons/css/boxicons.min.css'
 import Header from './components/header/Header.jsx';
 import Home from './components/header/home/Home.jsx';
 import About from './components/header/About/About.jsx'
-import Skills from './components/Skill/skills.jsx'
+import Skills from './components/Skill/Skills.jsx'
 import Services from './components/Services/Services.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import Footer from './components/Footer/Footer.jsx'
