955520f023e0748591d0a00aa68a0a9c46869494
Basit <basitmanzoor438@gmail.com>
2024-09-27 17:18:18 +0530

    resolved deployment issues

--- src/components/header/About/About.jsx
+++ src/components/header/About/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './About.css'
+import './about.css'
 import AboutImg from "../../../assets/about.png"
 import CV from "../../../assets/CV.pdf"
 import Info  from "../about/Info"
